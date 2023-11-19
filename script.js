c0281d1a7ec9865ee0098e00266d726d824c937b
Maria <mariagracielaesquivelfernandez@gmail.com>
2023-11-19 05:47:22 -0300

    Update login button and like functionality

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-function estado(elemento) {
-    let isLogged = parseInt(elemento.getAttribute('bool-log')) === 1
+let log = document.querySelector('#log')
+let likeButton = document.querySelectorAll('.like')
 
-    elemento.innerText = isLogged ? "Logout" : "Login"
-    elemento.setAttribute('bool-log', isLogged ? 0 : 1)
+let add = document.querySelector('.add')
+
+function estado() {
+    let isLogged = parseInt(log.getAttribute('bool-log')) === 1
+
+    log.innerText = isLogged ? "Logout" : "Login"
+    log.setAttribute('bool-log', isLogged ? 0 : 1)
 
 }
 
 
-function like(elemento){
-    var contador = parseInt(elemento.getAttribute('cont-likes'))
+function like(i){
+    var contador = parseInt(likeButton[i].getAttribute('cont-likes'))
+    console.log(contador)
     contador ++;
-    elemento.setAttribute('cont-likes', contador)
-    elemento.innerText=`${contador} likes`
+    likeButton[i].setAttribute('cont-likes', contador)
+    likeButton[i].innerText=`${contador} likes`
     return alert('Ninja was liked')
 }
-function remove(elemento){
-    elemento.remove()
+function remove(){
+    add.remove()
 }
+
